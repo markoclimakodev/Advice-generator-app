96227611870646fba79ac5b5c6e221f0393ba1a1
mkclimako <mkclimako@gmail.com>
2022-08-27 19:22:01 -0300

    feat(Advice): Generate a new advice every time users press the generate advice button

---
+++ server.js
@@ -0,0 +1,18 @@
+import { AppError } from "./src/utils/AppError.js";
+const generateAdvice = document.querySelector("#generate_advice");
+const advice_id = document.querySelector("#advice_id");
+const advice = document.querySelector("#advice");
+
+function fetchApiData() {
+  fetch(`https://api.adviceslip.com/advice`)
+    .then((response) => response.json())
+    .then(({ slip }) => {
+      advice_id.innerHTML = slip.id;
+      advice.innerHTML = slip.advice;
+    })
+    .catch((error) => {
+      throw new AppError(error.message);
+    });
+}
+
+generateAdvice.addEventListener("click", fetchApiData);
